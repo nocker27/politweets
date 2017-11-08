300cdc1e90059b20778009fab0832caa68257b17
Slepzs <slepzs@gmail.com>
2017-11-08 13:27:11 +0100

    Alt er ændret drenge

---
+++ enhedslisten.php
@@ -0,0 +1,78 @@
+<?php include 'include/headermenu.php'; ?>
+<section>
+  <div class="jumbotron text-center">
+    <div class="container">
+      <div class="row">
+
+		  <div class="col-md-2"></div>
+
+			<div class="col-md-8">
+
+
+				<a class="twitter-timeline" data-lang="da" data-theme="light" href="https://twitter.com/folketinget/lists/enhedslisten?ref_src=twsrc%5Etfw">A Twitter List by folketinget</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
+			</div>
+        <div class="col-md-2"></div>
+
+      </div>
+    </div>
+  </div>
+</section>
+<hr>
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, magni, doloribus, possimus eum sapiente deleniti doloremque fugit ut expedita molestiae iusto debitis eveniet modi obcaecati ipsam quos quis labore dicta.</p>
+		<button type="button" class="btn btn-success">Get in touch</button>
+      </div>
+    </div>
+  </div>
+</section>
+<hr>
+<div class="section well">
+    <div class="container">
+   	  <div class="row">
+		<div class="col-lg-4 col-md-4">
+            <h3 class="text-center">WHO WE ARE</h3>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, ducimus, sit, quibusdam quidem recusandae veniam eos quod error nisi repellat excepturi laboriosam aspernatur suscipit possimus consectetur dolores nihil labore quas eius illo accusamus nulla sed blanditiis porro accusantium. Perspiciatis, perferendis!</h5>
+        </div>
+		<div class="col-lg-4 col-md-4">
+		  <h3 class="text-center">GET IN TOUCH</h3>
+          <address class="text-center">
+  <strong>MyCompany, Inc.</strong><br>
+  Sunny Autumn Plaza, Grand Coulee,<br>
+  CA, 91308-4075, US<br>
+  <abbr title="Phone">P:</abbr> (123) 456-7890
+</address>
+</div>
+		<div class="col-lg-4 col-md-4">
+		  <h3 class="text-center">NEWSLETTER</h3>
+          <form>
+    <div class="form-group col-lg-9 col-md-8 col-sm-10 col-xs-10">
+<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+<button type="submit" class="btn btn-default">Subscribe<br>
+</button>
+</form>
+		</div>
+</div>
+    </div>
+	</div>
+    <footer class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <p>Copyright © MyCompany. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.3.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,1 @@
+<?php include 'include/headermenu.php'; ?>
